649f616690236b341d1d0e5fade6aa0afe8b9fe5
snofamv <74159423+snofamv@users.noreply.github.com>
2022-07-01 15:12:32 -0400

    Update config.php

--- config/config.php
+++ config/config.php
@@ -19,10 +19,10 @@ function autoLoad()
     require_once("libs/app.php");
 }
 
-define("URLBASE", "http://localhost:80");
+define("URLBASE", "https://portal-curricular.herokuapp.com");
 define("BASETITLE", "Portal curricular");
-define("USERDB", "root");
-define("PASSDB", "");
-define("HOSTDB", "localhost");
-define("NAMEDB", "test");
+define("USERDB", "b5c77f95f075d6");
+define("PASSDB", "c143cf89");
+define("HOSTDB", "us-cdbr-east-06.cleardb.net");
+define("NAMEDB", "heroku_60415f786969d9d");
 define("CHARSETDB", "utf8mb4");
