b4cb7a37a942dee4e556598fb8de689a8829db7f
snofamv <74159423+snofamv@users.noreply.github.com>
2022-07-08 13:34:22 -0400

    Update actualizar.php

--- views/panel/actualizar.php
+++ views/panel/actualizar.php
@@ -43,20 +43,21 @@ require "views/includes/navbar.user.php";
                             <option value="La Calera">La Calera</option>
                             <option value="Viña del Mar">Viña del Mar</option>
                             <option value="Valparaiso">Valparaiso</option>
+                            <option value="Quillota">Quillota</option>
                         </select>
                     </div>
 
                     <div class="col">
 
-                        <label class="label" for="carreras">Nueva Carrera</label>
-                        <select class="form-select" name="carreras" id="carreras" required>
-                            <option selected disabled hidden>Selecciona una carrera</option>
-                            <option value="Tecnico en Informatica">Tecnico en informatica</option>
-                            <option value="Tecnico en Enfermeria">Tecnico en enfermeria</option>
-                            <option value="construccion">Construccion</option>
-                            <option value="gastronomia">Gastronomia</option>
-                            <option value="Administracion de Empresas">Administrador de Empresas</option>
-                        </select>
+                         <label class="label m-2" for="carreras"  style="color:#FFFFFF;">Carrera</label>
+                    <select class="form-select" name="carreras" id="carreras">
+                        <option selected disabled hidden>Selecciona una carrera</option>
+                        <?php 
+                            foreach ($d as $sede) {
+                                echo "<option value='".$sede['carrera']."'>".$sede['carrera']."</option>";
+                            }
+                        ?>
+                    </select>
                     </div>
 
                 </div>
