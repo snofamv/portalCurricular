7c89e0051636e8d1759f89807bd7a1116d61cf06
snofamv <74159423+snofamv@users.noreply.github.com>
2022-07-25 14:09:08 -0400

    fx

--- controllers/lector_controller.php
+++ controllers/lector_controller.php
@@ -14,11 +14,23 @@ class LectorController extends SessionController
     {
         $this->vista->render("lector/buscar", []);
     }
+    private function calcularPaginas()
+    {
+        $query = $this->modelo->query("SELECT COUNT(*) AS total FROM data");
+        $this->total_datos = $query->fetch(PDO::FETCH_OBJ)->total;
+        $this->totalPaginas = ceil($this->total_datos / $this->resultadosPorPagina) + 2;
+    }
     public function lista()
     {
-        $this->vista->render("lector/lista", []);
+        $this->cargarModelo("paginacion");
+        $this->pagina_inicial = $_GET["pagina"] ?: 1;
+        $this->empezar_desde = ($this->pagina_inicial - 1) * $this->resultadosPorPagina;
+        $this->calcularPaginas();
+        $d["datos"] = $this->modelo->getDatos($this->empezar_desde, $this->resultadosPorPagina);
+        $d["nroDatos"] = $this->totalPaginas;
+        $this->vista->render("lector/lista", $d);
     }
-    
+
     public function salir()
     {
         parent::salir();
